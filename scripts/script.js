475aa3d95b1618f3d71d19bc2d7f3b8c2b295679
michavandijk <micha.vandijk@live.nl>
2020-10-25 13:13:17 +0100

    Add claps animation

--- scripts/script.js
+++ scripts/script.js
@@ -23,3 +23,54 @@ if (prevButton) { // only call this line when 'prevButton' is available
 prevButton.addEventListener("click", goToPrevItem);
 nextButton.addEventListener("click", goToNextItem);
 }
+
+
+// CLAPS ANIMATION
+
+const clapsContainer = document.querySelector('.clap-container');
+const clickCounter = document.querySelector('.click-counter');
+const userClapsCounter = clickCounter.querySelector('.counter');
+const totalClapsCounter = document.querySelector('.total-counter');
+const particlesContainer = document.querySelector('.particles-container');
+let userClaps = 0;
+let totalClaps = 69;
+
+clapsContainer.addEventListener('click', function () {
+  clapsContainer.classList.add('has-clapped');
+
+  if (!clickCounter.classList.contains('is-animating')) {  // if clickCounter does NOT (!) contain class 'animateInOut' then call function
+    increaseUserClaps();
+    increaseTotalClaps();
+
+    animateClickCounter();
+  }
+
+  animateParticlesContainer();
+})
+
+function animateClickCounter() {
+  clickCounter.classList.add('is-animating');
+
+  setTimeout(function() {
+    clickCounter.classList.remove('is-animating');
+  }, 3010);
+}
+
+function increaseUserClaps () {
+  userClaps++;
+  userClapsCounter.innerHTML = '+' + userClaps;
+}
+
+function increaseTotalClaps () {
+  totalClaps++;
+  totalClapsCounter.innerHTML = totalClaps;
+}
+
+function animateParticlesContainer() {
+  particlesContainer.style.transform = 'rotate(' + Math.floor(Math.random() * (72 - 1  + 1)) + 1 + 'deg)';
+  particlesContainer.classList.add('is-animating');
+
+  setTimeout(function() {
+    particlesContainer.classList.remove('is-animating');
+  }, 1000);
+}
